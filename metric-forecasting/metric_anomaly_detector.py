57f59d8585f13cccfb7e7086523a0d25bd45a337
Yingbei Tong <tybalex@gmail.com>
2021-10-08 11:56:52 +0800

    Alert and UI (#3)
    * refine rules for alerts and add dashboard info
    
    * improve ui
    
    * bug fix to handle duplicated timestamp
    
    * logger level

--- metric-forecasting/metric_anomaly_detector.py
+++ metric-forecasting/metric_anomaly_detector.py
@@ -9,10 +9,11 @@ import pandas as pd
 import ruptures as rpt
 from metric_model import ArimaModel
 
+## configurable parameters
 RETRAINING_INTERVAL_MINUTE = 1
 ROLLING_TRAINING_SIZE = 1440
-MIN_TRAIN_SIZE = 10
-ALERT_THRESHOLD = 5
+MIN_TRAINING_SIZE = 10
+ALERT_SCORE_THRESHOLD = 5
 CPD_TRACE_BACK_TIME = 30  ## minutes
 LOOP_TIME_SECOND = 60.0  # unit: second, type: float
 
@@ -52,6 +53,7 @@ class MetricAnomalyDetector:
         self.metric_model = ArimaModel()
         self.anomaly_history = []
         self.start_index = 0
+        self.anomaly_start_time = None
         # start index of training data. update by CPD and rolling time-window. For example if there's 2000 data points in the list, start_index should be 560.
 
     def load(self, history_data):
@@ -65,16 +67,20 @@ class MetricAnomalyDetector:
         logger.debug(f"history data loaded : {len(self.metric_xs)}")
 
     def run(self, data):
-        self.train_and_predict()
         xs_raw, y_raw = data[0]["value"]
         xs_raw = (
             int(xs_raw) // LOOP_TIME_SECOND * LOOP_TIME_SECOND
         )  ## convert time to start of minute :00
         xs_new = datetime.fromtimestamp(float(xs_raw))
+        if xs_new in self.metric_xs:
+            logger.warning("ERROR: duplicated timestamp!")
+            return None
         y_new = float(y_raw)
 
+        self.train_and_predict()
         is_anomaly = 0
         is_alert = 0
+        alert_len = None
         json_payload = {
             "timestamp": xs_new.isoformat(),
             "is_anomaly": is_anomaly,
@@ -82,19 +88,40 @@ class MetricAnomalyDetector:
             "alert_score": 0,
             "is_alert": is_alert,
             "y": y_new,
-            "yhat": y_new,
-            "yhat_lower": y_new,
-            "yhat_upper": y_new,
+            "yhat": None,
+            "yhat_lower": None,
+            "yhat_upper": None,
             "confidence_score": 0,
+            "alert_id": self.anomaly_start_time,
+            "alert_len": alert_len,
         }
-        if len(self.metric_xs) >= MIN_TRAIN_SIZE:
+        if len(self.metric_xs) >= MIN_TRAINING_SIZE:
             y_pred, y_pred_low, y_pred_high = self.pred_history[len(self.metric_xs)]
             if self.metric_name == "disk_usage":
-                is_anomaly = 1 if y_new >= 80 else 0
+                y_pred = None
+                y_pred_high = 80
+                is_anomaly = 1 if y_new >= y_pred_high else 0  # trigger: above boundary
             else:
-                is_anomaly = 1 if (y_new < y_pred_low or y_new > y_pred_high) else 0
+                is_anomaly = (
+                    1 if (y_new < y_pred_low or y_new > y_pred_high) else 0
+                )  # trigger: above or below
 
-            is_alert = self.update_alert_score(is_anomaly)
+            if is_anomaly == 1:
+                if self.alert_score_counter == 0:
+                    self.anomaly_start_time = xs_raw
+                self.alert_score_counter += 1
+            else:
+                if self.alert_score_counter > 0:
+                    self.alert_score_counter -= 2
+                    if self.alert_score_counter <= 0:
+                        self.anomaly_start_time = None
+                        self.alert_score_counter = 0
+            if self.anomaly_start_time:
+                alert_len = f"{int((xs_raw - self.anomaly_start_time) // 60)} minutes"
+
+            if self.alert_score_counter >= ALERT_SCORE_THRESHOLD:
+                is_alert = 1
+
             if is_alert == 1:
                 logger.warning(
                     f"Alert for {self.metric_name} at time : {xs_new.isoformat()}"
@@ -107,9 +134,11 @@ class MetricAnomalyDetector:
             json_payload["is_anomaly"] = is_anomaly
             json_payload["is_alert"] = is_alert
             json_payload["alert_score"] = self.alert_score_counter
+            json_payload["alert_id"] = self.anomaly_start_time
+            json_payload["alert_len"] = alert_len
 
         else:  ## for first 10 mins, simply use y as pred values
-            self.pred_history.append((y_new, y_new, y_new))
+            self.pred_history.append((None, None, None))
 
         if self.metric_name == "cpu_usage" and is_alert == 0 and is_anomaly == 1:
             logger.debug("Experimental: correct anomaly value.")
@@ -151,13 +180,18 @@ class MetricAnomalyDetector:
             To emsemble them, we can either take the intersect or the union.
         """
         if is_anomaly == 1:
+            if self.alert_score_counter == 0:
+                self.anomaly_start_time = xs_raw
             self.alert_score_counter += 1
         else:
             if self.alert_score_counter > 0:
                 self.alert_score_counter -= 2
+                if self.alert_score_counter <= 0:
+                    alert_len = (xs_raw - self.anomaly_start_time) // 60
+                    self.anomaly_start_time = None
+                    self.alert_score_counter = 0
 
-        self.alert_score_counter = max(self.alert_score_counter, 0)
-        if self.alert_score_counter >= ALERT_THRESHOLD:
+        if self.alert_score_counter >= ALERT_SCORE_THRESHOLD:
             return 1
         return 0
 
@@ -187,11 +221,11 @@ class MetricAnomalyDetector:
         modeling includes train and predict.
         every time there's a new data point, train a new model and forecast one step in future.
         """
-        if len(self.metric_xs) >= MIN_TRAIN_SIZE:
-            training_dataseries = pd.Series(self.metric_y, self.metric_xs).asfreq(
+        if len(self.metric_xs) >= MIN_TRAINING_SIZE:
+            tdf = pd.Series(self.metric_y, self.metric_xs)
+            training_dataseries = (tdf[~tdf.index.duplicated(keep="first")]).asfreq(
                 freq="T"
-            )
-            # training_dataseries = pd.Series(self.metric_y, self.metric_xs)
+            )  ## TODO: better efficiency
             ## max training size should be [ROLLING_TRAINING_SIZE]
             if len(self.metric_y) - self.start_index > ROLLING_TRAINING_SIZE:
                 self.start_index = len(self.metric_y) - ROLLING_TRAINING_SIZE
